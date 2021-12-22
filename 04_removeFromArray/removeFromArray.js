8dacdde31b9b329a4d513603f8de51c2be45d6b0
AlvaroIHC <alva0741@gmail.com>
2021-12-22 13:02:32 -0400

    Completed up to exercise 7

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,15 @@
 const removeFromArray = function() {
-
+    array = arguments[0];
+    for(let i = 1; i < arguments.length; i++){
+        let elementToRemove = arguments[i];
+        for(let j = 0; j < array.length; j++){
+            if (elementToRemove === array[j]){
+                array.splice(j, 1);
+                j--;
+            }
+        }
+    }
+    return array;
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
+const sumAll = function(number1, number2) {
+    if (typeof number1 !== 'number' || typeof number2 !== 'number'){
+        return 'ERROR';
+    }
+    if (number1 < 0 || number2 < 0){
+        return 'ERROR';
+    }
 
+    let minorNumber = number2 < number1 ? number2: number1;
+    let majorNumber = number1 + number2 - minorNumber;
+    let sum = 0;
+    for(let i = minorNumber; i <= majorNumber; i++){
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
